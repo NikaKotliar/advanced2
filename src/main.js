fae53f9c20de4aae7edc9cee34cb5d87b5bbb467
Nika Kotliar <wanderer2805@gmail.com>
2022-11-25 19:45:54 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+/* eslint-disable */
+let result = []
+
+const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40
+}
+
+
+function orderByProps(obj, keys) {
+    let reversedKeys = keys.reverse();
+    let keysSorted = Object.keys(obj).sort(function (a, b) {
+        return reversedKeys.indexOf(b) - reversedKeys.indexOf(a) || a.localeCompare(b);
+    });
+    for (let i = 0; i < keysSorted.length; i++) {
+        let array1 = {
+            key: keysSorted[i],
+            value: obj[keysSorted[i]]
+        }
+        result.push(array1)
+    }
+}
+
+orderByProps(obj, ["name", "level"])
+
+console.log(result)
